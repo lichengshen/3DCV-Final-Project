28b34875d536984654d65a8bfb5dd939132addfa
tonio <114290168+Tonio991314@users.noreply.github.com>
2022-12-22 20:07:21 +0800

    Add files via upload

--- merge_ply_files.py
+++ merge_ply_files.py
@@ -40,8 +40,8 @@ import plyfile
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--folder_path", required=True)
-    parser.add_argument("--merged_path", required=True)
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
     args = parser.parse_args()
     return args
 
@@ -50,12 +50,12 @@ def main():
     args = parse_args()
 
     files = []
-    for file_name in os.listdir(args.folder_path):
+    for file_name in os.listdir(args.input):
         if len(file_name) < 4 or file_name[-4:].lower() != ".ply":
             continue
 
         print("Reading file", file_name)
-        file = plyfile.PlyData.read(os.path.join(args.folder_path, file_name))
+        file = plyfile.PlyData.read(os.path.join(args.input, file_name))
         for element in file.elements:
             files.append(element.data)
 
@@ -64,7 +64,7 @@ def main():
     merged_el = plyfile.PlyElement.describe(merged_file, 'vertex')
 
     print("Writing merged file")
-    plyfile.PlyData([merged_el]).write(args.merged_path)
+    plyfile.PlyData([merged_el]).write(args.output)
 
 
 if __name__ == '__main__':
